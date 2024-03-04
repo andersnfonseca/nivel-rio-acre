ad41328cf3060084321b0bc5b6334391d5137214
Anderson Assunção <andersnn7@gmail.com>
2024-03-04 15:37:10 -0500

    loading

--- api/public/script.js
+++ api/public/script.js
@@ -1,25 +1,35 @@
 const nivel = document.getElementById('nivel');
 const horario = document.getElementById('horario');
 const title = document.getElementById('title');
+const loading = document.querySelector('.loading');
+
 const API_URL = 'https://nivelrioacreapi.vercel.app/';
 
 const getNivelAndHour = async () => {
+    try {
+        const response = await fetch(`${API_URL}v1/api`);
+        const data = await response.json();
+        
+        nivel.textContent = (data.Nivel / 100).toFixed(2) + 'm';
+        title.textContent = 'Nível do Rio Acre Hoje:';
+        
+        const horarioData = new Date(data.Horario);
+        horarioData.setHours(horarioData.getHours() - 2);
+        const dia = String(horarioData.getDate()).padStart(2, '0');
+        const mes = String(horarioData.getMonth() + 1).padStart(2, '0');
+        const ano = horarioData.getFullYear();
+        const hora = String(horarioData.getHours()).padStart(2, '0');
+        const minutos = String(horarioData.getMinutes()).padStart(2, '0');
 
-    const response = await fetch(`${API_URL}v1/api`);
-    const data = await response.json();
-    nivel.innerHTML = (data.Nivel / 100).toFixed(2) + 'm';
-    title.innerHTML = 'Nível do Rio Acre Hoje:';
-    
-    const horario = new Date(data.Horario);
-    horario.setHours(horario.getHours() - 2);
-    const dia = String(horario.getDate()).padStart(2, '0');
-    const mes = String(horario.getMonth() + 1).padStart(2, '0');
-    const ano = horario.getFullYear();
-    const hora = String(horario.getHours()).padStart(2, '0');
-    const minutos = String(horario.getMinutes()).padStart(2, '0');
+        const horarioFormatado = `Última atualização em ${dia}/${mes}/${ano} às ${hora}:${minutos}h`;
 
-    const horarioFormatado = `Última atualização em ${dia}/${mes}/${ano} às ${hora}:${minutos}h`;
-
-    document.getElementById('horario').innerHTML = horarioFormatado;
+        horario.textContent = horarioFormatado;
+        loading.style.display = 'none'; 
+        main.style.display = 'block'; 
+    } catch (error) {
+        console.error('Erro ao obter dados da API:', error);
+        loading.innerHTML = 'Erro ao carregar os dados.'; 
+    }
 }
+
 getNivelAndHour();
