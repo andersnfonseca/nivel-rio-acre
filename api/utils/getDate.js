0a86487fa12ea8f46db81d736bf5915237c192a4
Anderson Assunção <36905253+andersnfonseca@users.noreply.github.com>
2024-08-26 04:26:01 -0300

    Update getDate.js

--- api/utils/getDate.js
+++ api/utils/getDate.js
@@ -8,7 +8,7 @@ function getToday() {
 
 function getYersteday() {
     const date = new Date();
-    date.setDate(date.getDate() - 1);
+    date.setDate(date.getDate() - 3);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
